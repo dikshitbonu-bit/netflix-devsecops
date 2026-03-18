b2072a87d25fd7630ceb3e1d94d3cf11872cc546
dikshitbonu <dikshitbonu@gmail.com>
2026-03-18 10:44:03 +0000

    added changes to server.js

--- netflix-clone/backend/server.js
+++ netflix-clone/backend/server.js
@@ -59,7 +59,7 @@ app.use((req, res) => {
 });
 
 // Error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
